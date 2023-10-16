b9d633c90734561b7f4d30f1dba52a0f79da30e3
Prayag887 <prayagdhakal123@gmail.com>
2023-10-16 10:45:05 +0545

    first commit

---
+++ lib/pages/util/search_bar.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:music_app/pages/music_page.dart';
+
+class CustomSearchDelegate extends SearchDelegate {
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    // TODO: implement buildActions
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    // TODO: implement buildLeading
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // TODO: implement buildResults
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // TODO: implement buildSuggestions
+    throw UnimplementedError();
+  }
+}
+
+@override
+List<Widget>? buildActions(BuildContext context) {
+  // TODO: implement buildActions
+  throw UnimplementedError();
+}
+
+@override
+Widget? buildLeading(BuildContext context) {
+  // TODO: implement buildLeading
+  throw UnimplementedError();
+}
+
+@override
+Widget buildResults(BuildContext context) {
+  // TODO: implement buildResults
+  throw UnimplementedError();
+}
+
+@override
+Widget buildSuggestions(BuildContext context) {
+  // TODO: implement buildSuggestions
+  throw UnimplementedError();
+}
